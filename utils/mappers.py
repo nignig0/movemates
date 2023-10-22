1ad54ac5c5499fda720cd0146626eec3e9bcbc49
nignig0 <tanitoluwaadebayo@gmail.com>
2023-10-22 15:29:57 +0000

    hopefully this fixes the error

--- utils/mappers.py
+++ utils/mappers.py
@@ -14,7 +14,7 @@ def documentToTrip(dbTrip: Trip):
         'travel_buddies':  [str(buddy) for buddy in dbTrip['travel_buddies']] if dbTrip['travel_buddies'] else [],
         'estimated_cost_of_trip': dbTrip['estimated_cost_of_trip'],
         'active': dbTrip['active'],
-        'created_at': str(dbTrip['created_at']),
-        'updated_at': str(dbTrip['updated_at'])
+        'created_at': None if not dbTrip['created_at'] else dbTrip['created_at'],
+        'updated_at': None if not dbTrip['updated_at'] else dbTrip['updated_at'],
 
     }
